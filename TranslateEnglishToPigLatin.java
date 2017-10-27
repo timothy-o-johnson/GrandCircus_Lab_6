54697d3d99f04a31aa53667f24191741e886ba7d
Timothy Johnson <pavementii@gmail.com>
2017-10-27 10:35:54 -0400

    minor changes added comments and repeat loop

--- TranslateEnglishToPigLatin.java
+++ TranslateEnglishToPigLatin.java
@@ -17,9 +17,15 @@ public class TranslateEnglishToPigLatin {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String userInput = "default";
+		boolean repeat = true;
+		
+		System.out.print("Hey, I'm your Pig Latin Translator!");
+	
+		while(repeat){
+		
+		System.out.println("\nEnter a line to be translated: ");
 
-		System.out.print("Hey, you! Enter a line to be translated: ");
-
+		
 		userInput = convertWordToLowercase(sc.nextLine()); // converts word to lowercase
 
 		if (isBeginningOfWordAVowel(userInput)) { //checks if beginning of word is a vowel
@@ -28,15 +34,12 @@ public class TranslateEnglishToPigLatin {
 			System.out.println(translateToPigLatin(userInput)); //translates a word that begins with a consonant to PigLatin
 		}
 
-		/*
-		 * convertWordToLowercase(userInput); // convert each word to lowercase before
-		 * translating isBeginningOfWordAVowel(userInput); addWay(userInput); // add
-		 * "way" onto any word starting with a vowel moveConsonantsAndAddAy(userInput);
-		 * // if word starts with consonant, move all //consonants that appear before
-		 * the // first vowel to the end of the word and then add "ay" to the end of the
-		 * word
-		 */
-
+		System.out.println("\nWould you like to translate another word?");
+		repeat = repeatYesNo(sc);
+		
+		}
+		
+		sc.close();
 	}
 
 	public static String convertWordToLowercase(String string) {
@@ -60,16 +63,15 @@ public class TranslateEnglishToPigLatin {
 		// find the ConstantsBeforeVowel
 		// find index of the vowel, use this to
 		// return just the letters of after the vowel
-		// add the beggingConstantBeforeVowel to end of costantsAfterVowel and add "-ay"
-		
-		String beggingConstantsBeforeVowel = returnConsonantsBeforeVowel(userInput);
-		int indexOfVowel = beggingConstantsBeforeVowel.length();
+		// add the beginningConstantBeforeVowel to end of costantsAfterVowel and add "-ay"
+
+		String beginningConstantsBeforeVowel = returnConsonantsBeforeVowel(userInput);
+		int indexOfVowel = beginningConstantsBeforeVowel.length();
 		String lettersAfterVowel = returnLettersAfterTheVowel(userInput, indexOfVowel);
 
-		String ordwayInwayIgpayAtinlay = lettersAfterVowel + beggingConstantsBeforeVowel + "ay";
+		String ordwayInwayIgpayAtinlay = lettersAfterVowel + beginningConstantsBeforeVowel + "ay";
 
 		return ordwayInwayIgpayAtinlay;
-
 
 	}
 
@@ -89,25 +91,37 @@ public class TranslateEnglishToPigLatin {
 
 	public static String returnConsonantsBeforeVowel(String userInput) {
 		// take in a String return the consonants before the first vowel
-		
+
 		String newWord = "";
 		String tempChar;
 
 		for (int i = 0; i < userInput.length(); i++) { // loop through each char in the String
-			if (!isCharAVowel(userInput.charAt(i))) {  // check that the char is a vowel
+			if (!isCharAVowel(userInput.charAt(i))) { // check that the char is a vowel
 				tempChar = String.valueOf(userInput.charAt(i)); // if not a vowel, save char as string
 				newWord = newWord + tempChar; // add character to list of consonants before vowel
 			} else {
 				return newWord; // when it reaches a vowel return the collect of consonants
 			}
 		}
-		
+
 		return newWord;
 
 	}
 
 	public static String returnLettersAfterTheVowel(String userInput, int indexOfVowel) {
-		// take an int that is the index of the vowel 
+		// take an int that is the index of the vowel
 		return userInput.substring(indexOfVowel); // return the the vowel and the letters after that vowel
 	}
+	
+	public static boolean repeatYesNo(Scanner sc) {
+		// returns true or false depending if the user enters 'n'
+			sc.hasNext();
+			
+			if(sc.nextLine().equalsIgnoreCase("N")) {
+				return false;
+			} else {
+				return true;
+			}
+			
+		}
 }
